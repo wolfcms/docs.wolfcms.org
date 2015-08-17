9f00ac62f51886bbc51c4a46e35fd4782617b0bf
Martijn <martijn.niji@gmail.com>
2015-08-17 15:41:08 +0200

    Move to readthedocs theme and cleanup

--- docs/themes/wolfcms/extra-javascript.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-    $("p.admonition-title").each(function(index, value) {
-        var str = $(this).text();
-        var res = '<i class="fa fa-exclamation-circle fa-fm"></i> &nbsp;'.concat(str);
-        $(this).html(res);        
-    });
-});
